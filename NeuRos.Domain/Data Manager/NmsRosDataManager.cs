e0915d32baaac5017f2b2b70771fa7de9dea30be
Agamkhr <48441609+Agamkhr@users.noreply.github.com>
2019-12-30 02:54:43 +0530

    Add files via upload

---
+++ NeuRos.Domain/Data Manager/NmsRosDataManager.cs
@@ -0,0 +1,63 @@
+﻿using NeuRos.Domain.Context;
+using NeuRos.Domain.Entities;
+using NeuRos.Domain.Repository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeuRos.Domain.Data_Manager
+{
+    public class NmsRosDataManager : IDataRepository<NmsRos>
+    {
+        public readonly NeuRosDbContext _neuTriviaDbContext;
+        public NmsRosDataManager(NeuRosDbContext neuRosDbContext)
+        {
+            _neuTriviaDbContext = neuRosDbContext;
+
+        }
+        public void Add(NmsRos entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(List<NmsRos> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(NmsRos entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public NmsRos Get(DateTime date)
+        {
+            throw new NotImplementedException();
+        }
+
+        public NmsRos Get(string id, int number)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<NmsRos> GetAll(DateTime date)
+        {
+            
+        }
+
+        public IEnumerable<NmsRos> GetAll(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<NmsRos> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(int id, NmsRos entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ NeuRos.Domain/Repository/IDataRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeuRos.Domain.Repository
+{
+    public interface IDataRepository<TEntity>
+    {
+        IEnumerable<TEntity> GetAll();
+        IEnumerable<TEntity> GetAll(DateTime Date);
+        TEntity Get(DateTime id);
+        TEntity Get(string id, int number);
+        void Add(TEntity entity);
+        void Add(List<TEntity> entities);
+        void Update(int id, TEntity entity);
+        void Delete(TEntity entity);
+    }
+}
